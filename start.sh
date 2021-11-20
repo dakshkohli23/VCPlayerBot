fd3138c36896ed064ec66cbfb1611e30b50471ba
Dlaize Kohli <dakshkohli23@gmail.com>
2021-11-20 20:48:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,9 +3,6 @@ if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
   git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
-else
-  echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
 fi
 cd /VCPlayerBot
 pip3 install -U -r requirements.txt
