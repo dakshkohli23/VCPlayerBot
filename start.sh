021f2c03efe2a52fbab6ff1c71b85d7dca3babe4
Dlaize Kohli <dakshkohli23@gmail.com>
2021-11-20 15:22:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/dakshkohli23/VCPlayerBot
+  git clone https://github.com/dakshkohli23/VCPlayerBot /VCPlayerBot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/dakshkohli23/VCPlayerBot
+  git clone https://github.com/dakshkohli23/VCPlayerBot -b $BRANCH /VCPlayerBot
 fi
+cd /VCPlayerBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
