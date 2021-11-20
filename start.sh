5b99192d3b027096ad99564075f9323cebdcc1e8
Dlaize Kohli <dakshkohli23@gmail.com>
2021-11-20 21:15:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 echo "Starting...."
-
+if [ -z $BRANCH ]
+then
+  echo "Cloning main branch...."
+  else
+  echo "Cloning $BRANCH branch...."
 fi
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
